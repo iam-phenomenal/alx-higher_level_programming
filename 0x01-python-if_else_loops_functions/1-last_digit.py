8dd8205bf551c0c37b538ebd8bcbc49a15febfdc
iam-phenomenal <103444282+iam-phenomenal@users.noreply.github.com>
2022-11-30 19:06:45 +0000

    Updated task 1 coding style

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -7,8 +7,10 @@ number = random.randint(-10000, 10000)
 remainder = number % 10
 
 if remainder > 5:
-    print("Last digit of {} is {} and is greater than 5".format(number, remainder))
+    print("Last digit of {} is {} and is greater than 5"
+          .format(number, remainder))
 elif remainder == 0:
     print("Last digit of {} is {} and is 0".format(number, remainder))
 else:
-    print("Last digit of {} is {} and is less than 5 and not 0".format(number, remainder))
+    print("Last digit of {} is {} and is less than 5 and not 0"
+          .format(number, remainder))
