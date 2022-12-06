721047f53844d5f76d5131c47390f24ae25c2652
iam-phenomenal <ayandiranibrahim@outlook.com>
2022-12-06 00:10:06 +0000

    Fixed task 3

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 
 def print_reversed_list_integer(my_list=[]):
-    max_idx = len(my_list) - 1
-    while max_idx >= 0:
-        print("{}".format(my_list[max_idx]))
-        max_idx -= 1
-    return
+    if my_list:
+        my_list.reverse()
+        for i in my_list:
+            print("{:d}".format(i))
+    else:
+        return (None)
