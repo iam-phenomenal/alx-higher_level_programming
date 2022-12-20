2acda9a43d30cf8963e21ae58c9697095c4c4a9f
iam-phenomenal <103444282+iam-phenomenal@users.noreply.github.com>
2022-12-20 15:48:59 +0000

    Added task 3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+"""This module focuses on ALX task 3 [Python Classes]
+"""
+
+
+class Square:
+    """Creating a class to check for input TypeError
+    and ValueError
+    """
+
+    def __init__(self, size=0):
+        """
+        Args:
+            size: Size of the square(int)
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        else:
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.size = size
+
+    def area(self):
+        """Computes area of a square
+            Return:
+                Area of square [INT]
+        """
+        return self.size ** 2
