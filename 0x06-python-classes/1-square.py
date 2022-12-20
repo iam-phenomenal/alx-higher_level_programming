be25b137d740d9854d149b8e8960671aa9596d67
iam-phenomenal <103444282+iam-phenomenal@users.noreply.github.com>
2022-12-20 15:29:59 +0000

    Added Task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""This module focuses on ALX task 1:
+    Constraint:
+        A Square class
+        Private instance attribute of size
+"""
+
+
+class Square:
+    """Creating the square class based on the constraint above
+    """
+
+    def __init__(self, size):
+        self.size = size
