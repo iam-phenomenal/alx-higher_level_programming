b67c75781708daf831415051b5ad4446a069b65f
iam-phenomenal <103444282+iam-phenomenal@users.noreply.github.com>
2022-12-20 15:21:46 +0000

    Added documentation

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
-"""A module for working with squares.
+"""Creating a square class
 """
 
 
 class Square:
-    """Represents a 2D Polygon with 4 equal and perpendicular sides.
+    """Creating an empty class takes no attributes
     """
     pass
