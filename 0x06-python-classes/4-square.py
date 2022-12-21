ffa9a1b9e19a704fe0890a8651c74680def6e2ba
iam-phenomenal <103444282+iam-phenomenal@users.noreply.github.com>
2022-12-21 02:12:12 +0000

    Playing with Getters and Setters

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+
+"""ADDING GETTERS AND SETTERS TO SQUARE CLASS
+"""
+
+
+class Square:
+    """Initializing class
+    """
+
+    def __init__(self, size=0):
+        """
+        Args:
+            size: Size of the square(int)
+        """
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        else:
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self._size = size
+
+    def area(self):
+        """Computes area of a square
+            Return:
+                Area of square [INT]
+        """
+        return self._size ** 2
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self._size = value
+
+
