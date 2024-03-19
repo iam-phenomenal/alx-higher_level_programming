b428b92cf31e2a05f19cbeb52bebb76dc442c4ee
Ayandiran Ibrahim (iam-phenomenal) <ayandiranibrahim@outlook.com>
2024-03-19 01:56:57 +0100

    fix: Unknown error I guess

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -2,8 +2,5 @@
 
 def print_list_integer(my_list=[]):
     for i in my_list:
-        if(i == my_list[-1]):
-            print("{:d}".format(i))
-        else:
-            print("{:d}".format(i), end="\n")
+        print("{:d}".format(i))
     return
